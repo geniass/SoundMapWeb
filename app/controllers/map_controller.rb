04efcb027017136d5ab1589d5ffcd9fef5e196b3
Ari <aricroock@gmail.com>
2013-05-07 21:36:50 +0200

    bhhjbghugh

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,10 +4,12 @@ class MapController < ApplicationController
 
   def receive
     cs = CoordinateSet.create
+    puts params[:coords]
     params[:coords].each do |key,value|
       puts value
       cs.coordinates.new(value)
     end
+    cs.save
     respond_to do |format|
       format.json { render :json => params }
     end
